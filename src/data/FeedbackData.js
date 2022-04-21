222b2a1b0d490e7175c9621d2ce5b8fec97ee0a5
Kehinde Ajibola <t@LordWesley-Macbook-Pro.local>
2022-04-21 15:07:41 +0100

    Modified without backend

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est quaerat! Minus, harum! Omnis placeat eaque officiis, labore dolor quasi vel quod consequatur, error sed dolorum quis, autem nostrum in.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est quaerat! Minus, harum! Omnis placeat eaque officiis, labore dolor quasi vel quod consequatur, error sed dolorum quis, autem nostrum in.",
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est quaerat! Minus, harum! Omnis placeat eaque officiis, labore dolor quasi vel quod consequatur, error sed dolorum quis, autem nostrum in.",
+  },
+];
+
+export default FeedbackData;
